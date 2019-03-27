cc4da1659e18fea3e3c9824b2c41a40d05aed851
ctf <ctf@rock-chips.com>
2019-03-27 14:03:31 +0800

    cypress_bsa: change bsa version info string
    Change-Id: I88ff786cb8ad2e059bfe61fcf11ae9a7436df6cf
    Signed-off-by: ctf <ctf@rock-chips.com>

--- 3rdparty/embedded/brcm/bsa/common/bsa_version.c
+++ 3rdparty/embedded/brcm/bsa/common/bsa_version.c
@@ -12,4 +12,4 @@
 #include "bt_types.h"
 
 const UINT8 bsa_version_string[] = "BSA0107_00.26.00";
-const UINT8 bsa_version_info_string[] = "amlogic_kernel4.9_init_20170406";
+const UINT8 bsa_version_info_string[] = "rockchip_kernel4.4_init_20170406";
